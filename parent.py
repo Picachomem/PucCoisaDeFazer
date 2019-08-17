ae0537d486549076d7be81bf29eb03ecf1962fea
Picachomem <bentomaranhao@gmail.com>
2019-08-17 11:39:56 -0300

    bla

--- parent.py
+++ parent.py
@@ -1,6 +1,18 @@
 
-##  BEGIN MAIN
+##  BEGIN MAIN  ##
+
 
 class User:
 
-    
+    def __init__(self, userName, memberAmount, foodArray, foodAmount):
+        self.userName = userName
+        self.memberAmount = memberAmount
+        self.foodArray = foodArray
+        self.foodAmount = foodAmount
+
+#    frequency
+
+
+obj1 = User("Pedro", 4, ["leite", "feijao"], [1, 2])
+
+print(obj1.userName, obj1.memberAmount, obj1.foodArray, obj1.foodAmount)
