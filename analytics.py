20eef2f33b542fe7796033987bd1fa44c15c00a6
DiscoUlysses <miguelsilva@poli.ufrj>
2019-08-17 18:09:25 -0300

    Create analytics.py

---
+++ analytics.py
@@ -0,0 +1,9 @@
+import random
+def mergulha_registrinho(foodAmount):
+    wasteNot=[]
+    for i in range(len(foodAmount)):
+        wasteNot.append(random.randint(0,1))
+        print (wasteNot)
+    
+    
+    
