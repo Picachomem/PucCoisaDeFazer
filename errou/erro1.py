6d91cb6ab7681d40c6d59d5e6f976bb22c509a5d
DiscoUlysses <miguelsilva@poli.ufrj>
2019-08-17 11:26:08 -0300

    Create erro1.py

---
+++ errou/erro1.py
@@ -0,0 +1,13 @@
+import null
+def notNull(array):
+    if array==None:
+        return "nullarray"
+    else:
+        for i in array:
+            if i==None:
+                return "nullitem"
+            else:
+                pass
+            
+
+        
