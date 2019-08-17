e63913ac1938860bc00dde0d462cad11ebabd6b0
velozo27 <pa.velozo27@gmail.com>
2019-08-17 13:05:22 -0300

    Testes nome comida de novo

--- testes.py
+++ testes.py
@@ -1,5 +1,11 @@
 def cria_arq(lista):
+
+    arq = open('ArqListas.txt','r')
+    
     arq=open('ArqListas.txt','a')
+
+    arq.seek(0)
+    
 
     arq.write("~")
     arq.write(lista[0])
@@ -8,42 +14,20 @@ def cria_arq(lista):
     arq.write("\n")
     for index in range(len(lista[2])):
         arq.write(lista[2][index])
-        arq.write(";")
+        arq.write(" ")
     arq.write("\n")
     for index in range(len(lista[3])):
         arq.write(str(lista[3][index]))
-        arq.write(";")
+        arq.write(" ")
     arq.write("\n")
     
-#    arq.write(str(lista[1]))
- #   arq.write("\n")
 
     return
-        
+  
 listaTeste=['Daniel Coelho',4,['Arroz','Feijão'],[7,5]]
 listaTeste2=['Pedro Antônio Tibau Velozo',5,['Arroz','Macarrão','Maçã','Uva','Carne'],[3,4,5,6,6]]
 cria_arq(listaTeste)    
 cria_arq(listaTeste2)
 
 
-
-
-#lista = ['nome',5,['c', 'e'],[1, 2]]
-    
-'''
-def cria_arq(lista):
-    arq=open('ArqListas.txt','a')
-
-    arq.write("~", lista[0],"\n")
-    arq.write("~", lista[0],"\n")
-    arq.write("~", lista[0],"\n")
-    arq.write(lista[1], "\n")
-    for index in range(len(lista[2])):
-        arq.write(lista[2][index],";")
-    arq.write("\n")
-    for index in range(len(lista[3])):
-        arq.write(lista[3][index],";")
-    arq.write("\n")
-    
-    arq.write(lista[1], "\n")
-'''
+#def le_arq():
