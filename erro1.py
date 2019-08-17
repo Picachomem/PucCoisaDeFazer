34531c46e068ec90acc6b484f0d118c90a69aa4c
DiscoUlysses <miguelsilva@poli.ufrj>
2019-08-17 11:35:08 -0300

    Update erro1.py

--- erro1.py
+++ erro1.py
@@ -1,13 +1,19 @@
-import null
-def notNull(array):
-    if array==None:
-        return "nullarray"
-    else:
-        for i in array:
-            if i==None:
-                return "nullitem"
-            else:
-                pass
-            
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:22:17) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:\Dados\Miguel\PucCoisaDeFazer\erro1.py =============
+>>> notNull([])
+>>> notNull([None])
+'nullitem'
+>>> notNull([None])
+'nullitem'
+>>> 
+>>> notNull()
 
-        
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    notNull()
+TypeError: notNull() takes exactly 1 argument (0 given)
+>>> notNull(None)
+'nullarray'
+>>> 
